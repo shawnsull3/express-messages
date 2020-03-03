d92b800a6869de3cbb4b3feb61297fb7c622c486
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-03 12:24:04 -0700

    create backbone for all CRUD api requests

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const pool = require('./db/pgconfig'); // if you are using postgres, uncomment this line
 
-// if you are using mongo/mongoose uncomment this line
-// const Message = require('./db/Message');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-// if you are using postgres, uncomment this line
-// const pool = require('./db/pgconfig');
+
 
 app.use((req,res,next) => {
   res.status(404).send('That route does not exist');
 });
+
+app.post('/api/messages', (req,res) => {
+
+});
+
+app.get('/api/messages', (req,res) => {
+
+});
+
+
+///////all api requests below will need an addtional param on the path//////
+//get 1 message
+// app.get('/api/messages', (req,res) => {
+
+// });
+
+// app.put('/api/messages/', (req,res) => {
+
+// });
+
+// app.delete('/api/messages/', (req,res) => {
+
+// });
+
+
+
+
 
 const port = 3000;
 
@@ -18,3 +46,7 @@ app.listen(port, () => {
 });
 
 module.exports = app;
+
+
+// if you are using mongo/mongoose uncomment this line
+// const Message = require('./db/Message');
